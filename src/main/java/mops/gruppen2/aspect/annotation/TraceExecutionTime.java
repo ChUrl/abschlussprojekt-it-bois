bf715fe20afe8355a3c2f5407aad5bc12a70e080
Christoph <tobi@urpost.de>
2020-04-09 18:17:44 +0200

    add logging aspect + annotations

---
+++ src/main/java/mops/gruppen2/aspect/annotation/TraceExecutionTime.java
@@ -0,0 +1,15 @@
+package mops.gruppen2.aspect.annotation;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Schreibt die Methodenausführdauer in den Trace-Stream
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TraceExecutionTime {
+}
---
+++ src/main/java/mops/gruppen2/aspect/annotation/TraceMethodCall.java
@@ -0,0 +1,14 @@
+package mops.gruppen2.aspect.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Schreibt eine Nachricht bei Methodenausführung in den Trace-Stream
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TraceMethodCall {
+}
